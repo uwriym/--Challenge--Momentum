137978ad05ba7066c0784471c0702f538d5f69b0
uwriym <rim2wo@gmail.com>
2022-02-07 15:26:49 +0900

    Made Backgroung Image

---
+++ js/background.js
@@ -0,0 +1,21 @@
+const imageArr = [
+  "1.jpg",
+  "2.jpg",
+  "3.jpg",
+  "4.jpg",
+  "5.jpg",
+  "6.jpg",
+  "7.jpg",
+  "8.jpg",
+  "9.jpg",
+  "10.jpg",
+  "11.jpg",
+];
+
+const randomImage = imageArr[parseInt(Math.random() * imageArr.length)];
+
+const bgImage = document.createElement("img");
+bgImage.src = `img/${randomImage}`;
+document.body.prepend(bgImage);
+
+bgImage.classList.add("bgImage");
