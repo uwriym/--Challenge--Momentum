036b1128355b7e8d551c6dcab836d88e0b2255dd
uwriym <rim2wo@gmail.com>
2022-02-11 14:13:46 +0900

    add google search link

---
+++ js/search.js
@@ -0,0 +1,25 @@
+const searchForm = document.querySelector("#google-search");
+const searchInput = document.querySelector("#google-search input");
+const searchIcon = document.querySelector(
+  "#google-search .fa-magnifying-glass"
+);
+const googleIcon = document.querySelector("#google-search .fa-google");
+
+function handleInputVisible() {
+  searchInput.classList.remove("hidden");
+  googleIcon.classList.remove("no-visible");
+}
+function handleInputNoVisible() {
+  searchInput.classList.add("hidden");
+  googleIcon.classList.add("no-visible");
+}
+function handleSubmitSearchForm(event) {
+  event.preventDefault();
+  const inputValue = searchInput.value;
+  location.href = `https://www.google.com/search?q=${inputValue}`;
+}
+
+searchForm.addEventListener("mouseenter", handleInputVisible);
+searchForm.addEventListener("click", handleInputVisible);
+searchForm.addEventListener("mouseleave", handleInputNoVisible);
+searchForm.addEventListener("submit", handleSubmitSearchForm);
